94da489875bfbad5dbb1a3945ba4bdb2495b1c13
rrgroup <rrgroup@DESKTOP-VBGV8QM>
2018-07-10 08:41:42 -0400

    Sankar: added controller class with rest method

--- src/main/java/com/restaurantsapp/demo/controller/RestaurantController.java
+++ src/main/java/com/restaurantsapp/demo/controller/RestaurantController.java
@@ -1,22 +1,28 @@
-/**
- * 
- */
 package com.restaurantsapp.demo.controller;
 
-import com.restaurantsapp.demo.util.AppUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.restaurantsapp.demo.pojo.CustomerInformation;
 
 /**
  * @author User
  *
  */
+@RestController
 public class RestaurantController {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//AppUtil.customerInformationValidation(customerInformation)
+	@RequestMapping(value= "/fetchcustomerinformation", method= RequestMethod.GET, produces ="application/json")
+	public CustomerInformation fetchCustomerInformation() {
+		CustomerInformation  c = new CustomerInformation();
+		c.setFirstName("Sankar");
+		c.setLastName("k");
+		c.setAddress("Atlanta");
+		c.setEmailId("c@gmail.com");
+		c.setCustomerId(1001);
+		c.setPhoneNo("717-829-3232");
+		return c;
 	}
-
+	
 }
