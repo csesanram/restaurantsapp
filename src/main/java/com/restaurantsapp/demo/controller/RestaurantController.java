1e324b7853a10206c644a013d00f97adec174b12
rrgroup <rrgroup@DESKTOP-VBGV8QM>
2018-07-11 08:55:34 -0400

    Sankar: db details and added more function in restcontroller class

--- src/main/java/com/restaurantsapp/demo/controller/RestaurantController.java
+++ src/main/java/com/restaurantsapp/demo/controller/RestaurantController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.restaurantsapp.demo.pojo.CustomerInformation;
+import com.restaurantsapp.demo.pojo.Transaction;
 
 /**
  * @author User
  *
  */
 @RestController
+@RequestMapping("/restaurants")
 public class RestaurantController {
 
 	@RequestMapping(value= "/fetchcustomerinformation", method= RequestMethod.GET, produces ="application/json")
@@ -23,6 +25,22 @@ public class RestaurantController {
 		c.setCustomerId(1001);
 		c.setPhoneNo("717-829-3232");
 		return c;
+	}
+	
+	@RequestMapping(value= "/fetchtransaction", method = RequestMethod.GET, produces = "application/json")
+	public Transaction fetchTransaction() {
+		Transaction trans = new Transaction();
+		trans.setId(1001);
+		CustomerInformation  c = new CustomerInformation();
+		c.setFirstName("Sankar");
+		c.setLastName("k");
+		c.setAddress("Atlanta");
+		c.setEmailId("c@gmail.com");
+		c.setCustomerId(1001);
+		c.setPhoneNo("717-829-3232");
+		trans.setCi(c);
+		trans.setTotal(100, 1);
+		return trans;
 	}
 	
 }
